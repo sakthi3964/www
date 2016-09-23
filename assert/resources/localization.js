c7684d6446eb5f64eaf95e3a55d153162df6e708
sakthi3964 <sakthi3964@gmail.com>
2016-09-23 13:04:16 +0530

    modified helper file

--- assert/resources/localization.js
+++ assert/resources/localization.js
@@ -2,5 +2,103 @@ var localization = {
     username: {
         en: "Enter your mail-id",
         ta: "உங்கள் மின்னஞ்சல் ஐடி"
-    }
+    },
+    name: {
+        en: "The name should not be empty"
+    },
+    validname: {
+        en: "The name can consist of alphabetical characters and spaces only"
+    },
+    dob: {
+        en:"Enter your DOB"
+    },
+    validdob:{
+        en:"The date is not valid"
+    },
+    age:{
+        en:"Enter your age"
+    },
+    greaterage:{
+        en:"Still u are alive ??"
+    },
+    lesserage:{
+        en:"Your age should be greater than 18"
+    },
+    validno:{
+        en:"Enter valid number"
+    },
+    gender:{
+        en:"select your gender"
+    },
+    mobile_no:{
+        en:"Enter your mobile number"
+    },
+    address_line1:{
+        en:"enter your door NO"
+    },
+    address_line2: {
+        en:"Enter street name"
+    },
+    city: {
+        en:"enter your city"
+    },
+    state: {
+        en:"enter your state"
+    },
+     postal_code: {
+        en:"enter your postal_code"
+    },
+     reference: {
+        en:"select your Point of Contact"
+    },
+    password: {
+        en:"Enter you password"
+    },
+    psstr: {
+        en:"It must be more than 8 characters long"
+    },
+    psupper: {
+        en:"It must contain at least one upper case character"
+    },
+     pslower: {
+        en:"It must contain at least one upper case character"
+    },
+    psdigit: {
+        en:"It must contain at least one digit"
+    },
+    commitment: {
+        en:"Enter your commitment"
+    },
+    cv: {
+        en:"Upload your cv"
+    },
+    photo: {
+        en:"Upload your photo"
+    },
+    work_type: {
+        en:"Enter you work type"
+    },
+    designation: {
+        en:"Enter your designation"
+    },
+    organization: {
+        en:"Enter your organization"
+    },
+    area_of_expertise: {
+        en:"Enter your expertise"
+    },
+    course: {
+        en:"Enter your course"
+    },
+     experience: {
+        en:"Enter your experience"
+    },
+
+     department: {
+        en:"Enter your department"
+    },
+     institution: {
+        en:"Enter your institution"
+    },
+    
 }

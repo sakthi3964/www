cf5ce1878920b277772f0727a4b53e8db194109b
sakthi3964 <sakthi3964@gmail.com>
2017-03-19 21:24:04 +0530

    forgot password

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var BASEURL = "http://nkanaapi-sakthi3964.rhcloud.com/"; 
-var VOLUNTEER_IMAGE_URL = "http://nkanaapi-sakthi3964.rhcloud.com/uploads/volunteer/photo/";
-var MENTOR_IMAGE_URL = "http://nkanaapi-sakthi3964.rhcloud.com/uploads/mentor/photo/";
+// var VOLUNTEER_IMAGE_URL = "http://nkanaapi-sakthi3964.rhcloud.com/uploads/volunteer/photo/";
+// var MENTOR_IMAGE_URL = "http://nkanaapi-sakthi3964.rhcloud.com/uploads/mentor/photo/";
 
 // var BASEURL = "http://localhost:2500";
 // var VOLUNTEER_IMAGE_URL = "http://localhost:2500/uploads/volunteer/photo/";

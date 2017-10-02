40e3159fe4b0a1b7adc3d3bab17ceda6fd613c34
Nandhini2301 <nandhusam23@gmail.com>
2017-10-02 17:09:11 +0530

    forgot password added

--- app/mentor/mentorhome/js/mentorhome.js
+++ app/mentor/mentorhome/js/mentorhome.js
@@ -5,47 +5,53 @@ $(document).ready(function () {
     }
     else {
 
-         var home = {};
-            home.id = localStorage.getItem("user");
-            console.log(home.id);
-            httpPost("/viewchild", home, function (response) {
-                if(response == ""){
-                     console.log("response[1].approve_status");
-                }
-                console.log(response);
-                $(".loading_icon").addClass("hide");
-                if ((response == "") || (response[0].approve_status == 0)) {
-                    // $('#tracker').append('<div class="col-sm-4 col-xs-4"><p>Tracker<img src="../../../../helperfiles/img/tracker.png" class="img-responsive " alt="CHILD SELECTION" width="120" height="236"></p></div>');
-                    $('#view_tracker').removeClass('hide');
-                    $('#vreviewform').removeClass('hide');
-                    $('#viewgraph').removeClass('hide');
-                }
-                else {
-                    // $('#tracker').append('<a href="../../tracker/en/tracker.html"><div class="col-sm-4 col-xs-4"><p>Tracker<img src="../../../../helperfiles/img/tracker.png" class="img-responsive " alt="CHILD SELECTION" width="120" height="236"></p></div></a>');
-                    $('#viewtrack').removeClass('hide');
-                    $('#reviewform').removeClass('hide');
-                    $('#graph').removeClass('hide');
-                }
-            })
+        var home = {};
+        home.id = localStorage.getItem("user");
+        console.log(home.id);
+        httpPost("/viewchild", home, function (response) {
+            if (response == "") {
+                console.log("response[1].approve_status");
+            }
+            console.log(response);
+            $(".loading_icon").addClass("hide");
+            if ((response == "") || (response[0].approve_status == 0)) {
+                // $('#tracker').append('<div class="col-sm-4 col-xs-4"><p>Tracker<img src="../../../../helperfiles/img/tracker.png" class="img-responsive " alt="CHILD SELECTION" width="120" height="236"></p></div>');
+                $('#view_tracker').removeClass('hide');
+                $('#vreviewform').removeClass('hide');
+                $('#viewgraph').removeClass('hide');
+            }
+            else {
+                // $('#tracker').append('<a href="../../tracker/en/tracker.html"><div class="col-sm-4 col-xs-4"><p>Tracker<img src="../../../../helperfiles/img/tracker.png" class="img-responsive " alt="CHILD SELECTION" width="120" height="236"></p></div></a>');
+                $('#viewtrack').removeClass('hide');
+                $('#reviewform').removeClass('hide');
+                $('#graph').removeClass('hide');
+            }
+        })
         function mentorApproval() {
             var user = localStorage.getItem("user");
             var data = {};
             data.profileId = user;
             data.operation = "details";
-            console.log("my details",data);
+            console.log("my details", data);
             httpPost("/mentorapproval", data, function (response) {
-                console.log("hi",response);
+                console.log("hi", response.childrenId);
+                var childrenId = response.childrenId;
+                var enc = window.btoa(childrenId);
+                document.getElementById("modalviewprofile").onclick = function () { 
+                    window.location.href = "../../../admin/viewadminchildprofile/en/viewadminchildprofile.html?id:" + enc;
+                }
                 if (response.childrenResult.mentor_approval != null && response.childrenResult.mentor_approval == 0) {
                     $('#mentor-approval').modal({ backdrop: 'static', keyboard: false });
-                    $('#childname').append("You got request for the child "+response.childrenResult.full_name);
+                    $('#childname').append("You got request for the child " + response.childrenResult.full_name);
                 }
+                
 
             })
         }
         mentorApproval();
 
     }
-           
+
 
 
     $("#mentorapproval").click(function () {
--- shared/forgot_password/js/forgotpassword.js
+++ shared/forgot_password/js/forgotpassword.js
@@ -1,17 +1,35 @@
 
-function forgotPassword() {
-    var data = {};
-    data.dob = ($('#dob').val()).trim();
-    data.mobileNo = ($('#mobileNo').val()).trim();
-    data.email = ($('#email').val()).trim();
-    // if (data.dob != "" && data.mobileNo != "" && data.email != "") {
-    httpPost("/forgotPassword", data, function (response) {
-        if (response) {
-            console.log(JSON.stringify(response))
-        }
+
+$(document).ready(function () {
+    $("#dob").datepicker({
+
+        dateFormat: 'dd-mm-yy',
+        maxDate: new Date, minDate: new Date(1960, 1, 12)
+    });
+    $("#submitforget").click(function () {
+        console.log("happy");
+        var data = {};
+        data.dob = ($('#dob').val()).trim();
+        data.mobileNo = ($('#mobileNo').val()).trim();
+        data.email = ($('#email').val()).trim();
+        data.BASEURL = BASEURL;
+        // if (data.dob != "" && data.mobileNo != "" && data.email != "") {
+        httpPost("/forgotPassword", data, function (response) {
+
+            console.log(response);
+            if (response == 1) {
+                $('#emailnotsent').modal({ backdrop: 'static', keyboard: false });
+            }
+            else {
+                $('#emailsent').modal({ backdrop: 'static', keyboard: false });
+            }
+        });
     });
 
-    // }
 
-}
+
+});
+
+
+
 

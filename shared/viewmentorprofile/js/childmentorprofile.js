c855c9c6a98831f2320014fa06fbb99bab98a30f
Sakthi <sakthi@tycl.org.in>
2017-10-08 21:22:37 +0530

    child side mentor profile issues fixed

--- shared/viewmentorprofile/js/childmentorprofile.js
+++ shared/viewmentorprofile/js/childmentorprofile.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
                     }
                     else {
                         $("#blockreg2").removeClass("hide");
-                        $('#profileimage').append('<img src="' + profile_photo + element.profileinfo.photo + '" width="150" height="150">');
+                        $('#profileimage').append('<img src="' + profile_photo + response[1].profile.profileinfo.photo + '" width="150" height="150">');
                         $('#name').html(response[1].profile.name);
                         $('#role').html(response[1].profile.role);
                         $('#dob').html(day + "-" + month + "-" + year);

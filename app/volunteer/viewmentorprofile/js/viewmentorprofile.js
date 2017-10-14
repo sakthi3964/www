68875fe81becbd714da34eac4678f98f38e0e790
Sakthi <sakthi3964@gmail.com>
2017-10-14 21:16:42 +0530

    profile photo issues fixed

--- app/volunteer/viewmentorprofile/js/viewmentorprofile.js
+++ app/volunteer/viewmentorprofile/js/viewmentorprofile.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
             // response[0].profileinfo.photo="";
             var profileImageUrl = "";
             if(response[0].profileinfo.photo){
-              profileImageUrl = response[0].profile_photo + response[0].profileinfo.photo;
+              profileImageUrl = profile_photo + response[0].profileinfo.photo;
             }else{
               profileImageUrl= 'https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder-300-grey.jpg';
             }
